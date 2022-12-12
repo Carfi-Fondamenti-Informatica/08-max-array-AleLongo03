0a6ee29d63e2759591f6f8998ca900376d3d8197
AleLongo03 <116566037+AleLongo03@users.noreply.github.com>
2022-12-12 15:35:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
 
+#include "lib.h"
+
+float max (float pt[], int n)
+{ int i=0;
+    while (i<n)
+    {
+        if (pt[0]>pt[i])
+        {
+            i++;
+        }
+        else
+        {
+            pt[0]=pt[i];
+         i++;
+        }
+    }
+    return pt[0];
+}
